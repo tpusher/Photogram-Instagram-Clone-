cf7cdfb1d2caf14eefac68955e7d7b50cc15f835
tpusher <tpusher@clemson.edu>
2024-07-04 14:19:00 -0400

    Added All Files

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+    def 
+      def index
+        @ = .scoped
+      end
+    
+      def show
+        @ = .find(params[:id])
+      end
+    
+      def create
+        @ = .new(params[:])
+      end
+    
+      def update
+        @ = .find(params[:id])
+      end
+    
+      def edit
+        @ = .find(params[:id])
+      end
+    
+      def destroy
+         = .find(params[:id])
+      end
+end
